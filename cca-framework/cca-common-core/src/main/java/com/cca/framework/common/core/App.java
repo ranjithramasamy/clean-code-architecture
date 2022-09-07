95913e15d3512cbaf7f16bde37649c690488a8c9
Ranjith Ramasamy <ranjith.ramasamy@hotmail.com>
2022-09-07 23:01:56 +0530

    Initial code

---
+++ cca-framework/cca-common-core/src/main/java/com/cca/framework/common/core/App.java
@@ -0,0 +1,13 @@
+package com.cca.framework.common.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cca-framework/cca-webapi-srvc/webapi-srvc-common/src/main/java/com/cca/framework/webapi/common/App.java
@@ -0,0 +1,13 @@
+package com.cca.framework.webapi.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
